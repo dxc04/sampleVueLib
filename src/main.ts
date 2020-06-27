aacc93d827a9ef1203f51488ed91f36a5cf8ede2
Dixie Philamerah Atay <dixie.atay@gmail.com>
2020-06-27 13:07:16 +0800

    :art: Simplify export web components

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,7 @@ import Vue from "vue";
 // import components here
 import ColorContrast from "./components/ColorContrast.vue";
 
-interface Comp<TValue> {
-  [id: string]: TValue;
-}
-
-const Components: Comp<object> = {
-  ColorContrast
+// export components for this library
+export default {
+    ColorContrast
 };
-
-Object.keys(Components).forEach((name: string) => {
-  Vue.component(name, Components[name]);
-});
-export default Components;
