6f824ebb8a91a8c7f6faa38127b7e2e98cb87a6e
Dixie Philamerah Atay <dixie.atay@gmail.com>
2020-06-29 15:37:07 +0800

    :package: Added Test Component

---
+++ src/components/Test.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>This is a test component</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Test extends Vue {}
+</script>
+
